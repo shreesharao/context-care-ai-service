7094d5f0d6e5cfbc40cd8af96d567d2098d50830
srao <srao@terarecon.com>
2026-03-22 13:05:55 +0530

    Adding structured logging.

--- src/ContextCare.Api/Program.cs
+++ src/ContextCare.Api/Program.cs
@@ -6,6 +6,7 @@ using OpenTelemetry.Trace;
 using OpenTelemetry.Metrics;
 using Scalar.AspNetCore;
 using OpenTelemetry;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,16 +20,18 @@ builder.Services.AddOpenTelemetry()
         .AddAspNetCoreInstrumentation())
     .WithMetrics(metrics => metrics
         .AddAspNetCoreInstrumentation())
-    .UseOtlpExporter(OpenTelemetry.Exporter.OtlpExportProtocol.Grpc, 
+    .UseOtlpExporter(OpenTelemetry.Exporter.OtlpExportProtocol.Grpc,
                      Uri.TryCreate(builder.Configuration.GetValue<string>("openTelemetry:endpoint"),
                                    UriKind.Absolute,
-                                   out var uri) ? uri: throw new InvalidOperationException("Invalid OpenTelemetry endpoint")); // Automatically collect HTTP metrics
+                                   out var uri) ? uri : throw new InvalidOperationException("Invalid OpenTelemetry endpoint")); // Automatically collect HTTP metrics
 
 builder.Services.AddHealthChecks()
     .AddCheck("live", () => HealthCheckResult.Healthy())
-    
+
     // The "ready" check is a more complex check that might involve checking database connectivity, external service availability, etc.
     .AddAsyncCheck("ready", async () => HealthCheckResult.Healthy());
+
+builder.Host.UseSerilog((context, sp, config) => config.ReadFrom.Configuration(context.Configuration));
 
 var app = builder.Build();
 
@@ -44,12 +47,20 @@ if (app.Environment.IsDevelopment())
 
 app.MapHealthChecks("/livez", new HealthCheckOptions()
 {
-    Predicate = option => option.Tags.Contains("live")
+    Predicate = option => option.Tags.Contains("live"),
+    ResponseWriter = (context, report) =>
+    {
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogInformation("Health check result {@Report}", report);
+        return context.Response.WriteAsync(report.Status.ToString());
+    }
 });
 
 app.MapHealthChecks("/readyz", new HealthCheckOptions()
 {
     Predicate = check => check.Name.Equals("ready")
 });
+
+app.UseSerilogRequestLogging();
 
 await app.RunAsync();
